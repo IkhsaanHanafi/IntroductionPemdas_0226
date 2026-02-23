6a3bb2d55051a84d2271960f15d37994142bd72e
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2026-02-23 19:05:22 +0700

    first commit pxl

---
+++ pertemuan2part1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){ //mulai
+    int p, l, luas;
+
+    //langkah 1
+    cout << "Masukkan panjang: ";
+    cin >> p;
+
+    //langkah 2
+    cout << "Masukkan lebar: ";
+    cin >> l;
+
+    //langkah 3
+    luas = p * l;
+    cout << "Luas persegi panjang adalah: " << luas << endl;
+
+//selesai
+}
